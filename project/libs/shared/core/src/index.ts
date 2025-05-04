a97c9eb8382d020e3fda997074e2e538f401b4bc
Alexander Lyzhin <al.lyzhin@gmail.com>
2025-05-05 01:23:41 +0300

    Добавит libs/shared/core

---
+++ project/libs/shared/core/src/index.ts
@@ -0,0 +1,5 @@
+export { Entity } from './base/entity';
+
+export { UserRole } from './types/user-role.enum';
+export type { User } from './types/user.interface';
+export type { AuthUser } from './types/auth-user.interface';
