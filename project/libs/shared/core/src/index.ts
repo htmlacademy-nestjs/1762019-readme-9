d5f119a7aa147cfb4b62fdbe48af4a25cef76659
Alexander Lyzhin <al.lyzhin@gmail.com>
2025-05-06 00:33:36 +0300

    Добавит интерфейсы StorableEntity, EntityFactory

--- project/libs/shared/core/src/index.ts
+++ project/libs/shared/core/src/index.ts
@@ -3,3 +3,5 @@ export { Entity } from './base/entity';
 export { UserRole } from './types/user-role.enum';
 export type { User } from './types/user.interface';
 export type { AuthUser } from './types/auth-user.interface';
+export type { StorableEntity } from './types/storable-entity.interface';
+export type { EntityFactory } from './types/entity-factory.interface';
