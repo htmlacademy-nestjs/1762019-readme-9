efca58cde4429faf6064acb70fd4d53d3935fea5
Alexander Lyzhin <al.lyzhin@gmail.com>
2025-05-06 02:54:18 +0300

    добавит CreateUserDto, LoginUserDto

---
+++ project/libs/user/authentication/src/dto/create-user.dto.ts
@@ -0,0 +1,7 @@
+export class CreateUserDto {
+  public email = '';
+  public dateBirth = '';
+  public firstname = '';
+  public lastname = '';
+  public password = '';
+}
---
+++ project/libs/user/authentication/src/dto/login-user.dto.ts
@@ -0,0 +1,4 @@
+export class LoginUserDto {
+  public email = '';
+  public password = '';
+}
