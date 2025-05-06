33bc50c76f44e8da4a319f5ca4ae3d88e4810492
Alexander Lyzhin <al.lyzhin@gmail.com>
2025-05-06 03:37:49 +0300

    Добавит методы verifyUser, getUser в AuthenticationService

--- project/libs/user/authentication/src/authentication/authentication.service.ts
+++ project/libs/user/authentication/src/authentication/authentication.service.ts
@@ -1,11 +1,13 @@
 import dayjs from 'dayjs';
-import { ConflictException, Injectable } from '@nestjs/common';
+import { ConflictException, Injectable, NotFoundException, UnauthorizedException} from '@nestjs/common';
 
 import { UserRole } from '@project/core';
 import { BlogUserRepository, BlogUserEntity } from '@project/blog-user';
 
 import { CreateUserDto } from '../dto/create-user.dto';
-import { AUTH_USER_EXISTS } from './authentication.constants';
+import { AUTH_USER_EXISTS, AUTH_USER_NOT_FOUND, AUTH_USER_PASSWORD_WRONG } from './authentication.constants';
+import { LoginUserDto } from '../dto/login-user.dto';
+
 
 @Injectable()
 export class AuthenticationService {
@@ -33,5 +35,30 @@ export class AuthenticationService {
     const userEntity = await new BlogUserEntity(blogUser).setPassword(password)
 
     return this.blogUserRepository.save(userEntity);
+  }
+
+  public async verifyUser(dto: LoginUserDto) {
+    const { email, password } = dto;
+    const user = await this.blogUserRepository.findByEmail(email);
+
+    if (!user) {
+      throw new NotFoundException(AUTH_USER_NOT_FOUND);
+    }
+
+    if (!await user.comparePassword(password)) {
+      throw new UnauthorizedException(AUTH_USER_PASSWORD_WRONG);
+    }
+
+    return user;
+  }
+
+  public async getUser(id: string) {
+    const user = await this.blogUserRepository.findById(id);
+
+    if (!user) {
+      throw new NotFoundException(AUTH_USER_NOT_FOUND);
+    }
+
+    return user;
   }
 }
