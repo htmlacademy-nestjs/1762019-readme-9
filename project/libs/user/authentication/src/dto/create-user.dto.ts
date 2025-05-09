a2da013da222b4f45cebbc7a55ddaad3845f6e02
Alexander Lyzhin <al.lyzhin@gmail.com>
2025-05-10 02:09:05 +0300

    Дополнит документацию Open API

--- project/libs/user/authentication/src/dto/create-user.dto.ts
+++ project/libs/user/authentication/src/dto/create-user.dto.ts
@@ -3,30 +3,39 @@ import { ApiProperty } from '@nestjs/swagger';
 export class CreateUserDto {
   @ApiProperty({
     type: String,
+    description: 'User unique address',
     default: 'user@notfound.local',
+    example: 'user@user.ru'
   })
   public email!: string;
 
   @ApiProperty({
     type: String,
+    description: 'User birth date',
+    example: '1981-03-12',
     default: '2012-02-22',
   })
   public dateBirth!: string;
 
   @ApiProperty({
     type: String,
-    default: 'Keks',
+    description: 'User first name',
+    example: 'Keks',
   })
   public firstname!: string;
 
   @ApiProperty({
     type: String,
+    description: 'User last name',
     default: 'Smith',
+    example: 'Ivanov'
   })
   public lastname!: string;
 
   @ApiProperty({
     type: String,
+    description: 'User last name',
+    example: 'Ivanov',
     default: '123456',
   })
   public password!: string;
--- project/libs/user/authentication/src/dto/login-user.dto.ts
+++ project/libs/user/authentication/src/dto/login-user.dto.ts
@@ -2,12 +2,14 @@ import { ApiProperty } from '@nestjs/swagger';
 
 export class LoginUserDto {
   @ApiProperty({
+    description: 'User uniq email',
     type: String,
     default: 'user@notfound.local',
   })
   public email!: string;
 
   @ApiProperty({
+    description: 'User password',
     type: String,
     default: '123456',
   })
