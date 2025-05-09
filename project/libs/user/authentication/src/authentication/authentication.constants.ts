2bbee48faf8110f1b4a7fbd591838e2cadb99222
Alexander Lyzhin <al.lyzhin@gmail.com>
2025-05-10 00:55:51 +0300

    Объединит связанные переменные в AUTH_USER

--- project/libs/user/authentication/src/authentication/authentication.constants.ts
+++ project/libs/user/authentication/src/authentication/authentication.constants.ts
@@ -1,3 +1,5 @@
-export const AUTH_USER_EXISTS = 'User with this email exists';
-export const AUTH_USER_NOT_FOUND = 'User not found';
-export const AUTH_USER_PASSWORD_WRONG = 'User password is wrong';
+export const AUTH_USER = {
+  EXISTS: 'User with this email exists',
+  NOT_FOUND: 'User not found',
+  PASSWORD_WRONG: 'User password is wrong',
+} as const;
