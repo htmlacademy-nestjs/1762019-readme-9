73ff5c2ab0550e682e6e983c846790e219b8139a
Alexander Lyzhin <al.lyzhin@gmail.com>
2025-05-12 00:00:31 +0300

    Демо: чтение конфига с помощью декоратора @Inject

--- project/libs/user/authentication/src/authentication/authentication.service.ts
+++ project/libs/user/authentication/src/authentication/authentication.service.ts
@@ -1,6 +1,7 @@
 import dayjs from 'dayjs';
 import {
   ConflictException,
+  Inject,
   Injectable,
   NotFoundException,
   UnauthorizedException,
@@ -9,13 +10,24 @@ import {
 import { UserRole } from '@project/core';
 import { BlogUserRepository, BlogUserEntity } from '@project/blog-user';
 
+import { dbConfig } from '@project/user-config';
+
 import { CreateUserDto } from '../dto/create-user.dto';
 import { AUTH_USER } from './authentication.constants';
 import { LoginUserDto } from '../dto/login-user.dto';
+import type { ConfigType } from '@nestjs/config';
 
 @Injectable()
 export class AuthenticationService {
-  constructor(private readonly blogUserRepository: BlogUserRepository) {}
+  constructor(
+    private readonly blogUserRepository: BlogUserRepository,
+    @Inject(dbConfig.KEY)
+    private readonly databaseConfig: ConfigType<typeof dbConfig>
+  ) {
+    const { host, user } = databaseConfig;
+
+    console.log({ host, user });
+  }
 
   public async register(dto: CreateUserDto) {
     const { email, firstname, lastname, password, dateBirth } = dto;
