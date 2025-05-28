ae0a8e10b39f8a7342a5bb1598721d251bd5ca8c
Alexander Lyzhin <al.lyzhin@gmail.com>
2025-05-28 22:13:06 +0300

    Добавит валидацию в оставшиеся DTO

--- project/libs/blog/blog-category/src/lib/dto/create-category.dto.ts
+++ project/libs/blog/blog-category/src/lib/dto/create-category.dto.ts
@@ -1,9 +1,11 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { IsString } from 'class-validator';
 
 export class CreateCategoryDto {
   @ApiProperty({
     description: 'Uniq category name',
     example: 'flowers'
   })
+  @IsString()
   public title!: string;
 }
