c3574e770631e6bd7d62bbf49a339cc91f98d398
Alexander Lyzhin <al.lyzhin@gmail.com>
2025-05-29 17:37:50 +0300

    Добавит библиотеку @project/blog-comment

--- project/libs/shared/core/src/index.ts
+++ project/libs/shared/core/src/index.ts
@@ -6,3 +6,4 @@ export type { AuthUser } from './types/auth-user.interface';
 export type { StorableEntity } from './types/storable-entity.interface';
 export type { EntityFactory } from './types/entity-factory.interface';
 export type { Category } from './types/category.interface'
+export type { Comment } from './types/comment.interface';
