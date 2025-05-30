891085b2db51c40a6a81a8604a9ad86ae66ea8ba
Alexander Lyzhin <al.lyzhin@gmail.com>
2025-05-30 17:47:07 +0300

    Добавит функцию getJwtOptions. WIP Для работы с JWT в Nest мы воспользуемся пакетом `@nestjs/jwt`. Как и любой другой внешний модуль его необходимо настроить. В качестве настроек необходимо передать секрет, который будет использоваться для JWT и время жизни токена.
    Эту информацию можно передавать в процессе формирования подписи (при каждом вызове соответствующей функции), а можно один раз, при подключении модуля.
    
    Мы пойдём вторым путём, поэтому подготовим функцию `getJwtOptions`. Она вернёт `JwtModuleOptions`. Это настройки для nestjs/jwt.
    
    Передадим алгоритм, секретку и время жизни токена.

--- project/libs/user/config/src/index.ts
+++ project/libs/user/config/src/index.ts
@@ -3,3 +3,4 @@ export { UserConfigModule } from './user-config.module';
 export { applicationConfig } from './configs/app.config';
 export { dbConfig } from './configs/mongo.config';
 export { jwtConfig } from './configs/jwt.config';
+export { getJwtOptions } from './get-jwt-options';
