b869acfc1ccb937668814bda2c5d59340a1f3a22
Alexander Lyzhin <al.lyzhin@gmail.com>
2025-06-01 21:44:18 +0300

    Подключит ServeStaticModule в FileUploaderModule. Воспользуемся модулем `ServeStaticModule` в модуле `FileModule`. Подключение выполняется стандартным способом: либо методом `forRoot`, либо `forRootAsync`.
    Мы воспользуемся вторым вариантом, так как путь к директории со статичными файлами нам необходимо извлечь из настроек.
    
    В метод `forRootAsync` передадим объект: укажем сервис, который требуется внедрить в качестве зависимости (`inject:[ConfigService]`). Им мы воспользуемся при создании `ServerStaticModuleOptions`. Чтобы подготовить этот объект, мы опишем функцию `useFactory`. Это функция фабрики. В Nest она используется для создания и настройки экземпляров сервисов, провайдеров и других компонентов.
    
    Сигнатура функции `useFactory` выглядит следующим образом:
    
    ```
    useFactory: (...args: any[]) => T | Promise<T>
    ```
    
    Параметр типа `T` обозначает тип создаваемого экземпляра компонента, который будет возвращать фабрика. Аргументом `useFactory` принимает другие провайдеры, которы могут потребоваться для создания компонента.
    
    Мы передаём `ConfigService`, который позволит нам получить доступ
    к извлечённым настройкам. Сервис будет внедрён с помощью DI (см. `inject`).
    
    Результатом функции должен стать экземпляр компонента или промис, который зарезолвится в экземпляр компонента.
    
    Внутри функции мы извлекаем настройку к директории со статичными файлами. Затем возвращаем массив настроек для `ServerStaticModule`.
    
    Пока используем только две настройки:
    
    * `rootPath` — путь к директории со статичными файлами;
    * `serveRoot` — задаёт корневую директорию статических файлов. По умолчание в настройке пустая строка. Это означает, что для получения статического файла необходимо обратиться к корневому маршруту сервиса: `http://localhost:3000/file.txt`.
    
    Чтобы задать дополнительный сегмент, например, `static`, и предназначена опция `serveRoot`.

--- project/libs/file-vault/file-uploader/src/lib/file-uploader.module.ts
+++ project/libs/file-vault/file-uploader/src/lib/file-uploader.module.ts
@@ -1,11 +1,30 @@
 import { Module } from '@nestjs/common';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { ConfigService } from '@nestjs/config';
 
 import { FileUploaderController } from './file-uploader.controller';
 import { FileUploaderService } from './file-uploader.service';
 
+const SERVE_ROOT = '/static';
+
 @Module({
+  imports: [
+    ServeStaticModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => {
+        const rootPath = configService.get<string>(
+          'application.uploadDirectory'
+        );
+        return [
+          {
+            rootPath,
+            serveRoot: SERVE_ROOT,
+          },
+        ];
+      },
+    }),
+  ],
   controllers: [FileUploaderController],
   providers: [FileUploaderService],
-  exports: [],
 })
 export class FileUploaderModule {}
