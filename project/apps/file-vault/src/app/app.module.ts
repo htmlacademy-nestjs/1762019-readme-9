bbea3776bc0c4b1b02eff54a70f8fa9896b929ca
Alexander Lyzhin <al.lyzhin@gmail.com>
2025-06-01 22:24:03 +0300

    Добавит настройки для подключения к Mongo (file-vault) Для подключения к MongoDB, базе, в которой будет хранится информация о загруженных файлах, потребуются параметры. Добавим их в конфигурацию сервиса `file-vault`.

--- project/apps/file-vault/src/app/app.module.ts
+++ project/apps/file-vault/src/app/app.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 
 import { FileUploaderModule } from '@project/file-uploader';
-import { FileVaultConfigModule } from '@project/file-vault-config';
+import {
+  FileVaultConfigModule,
+  getMongooseOptions,
+} from '@project/file-vault-config';
 
 @Module({
-  imports: [FileVaultConfigModule, FileUploaderModule],
+  imports: [
+    FileVaultConfigModule,
+    FileUploaderModule,
+    MongooseModule.forRootAsync(getMongooseOptions()),
+  ],
 })
 export class AppModule {}
