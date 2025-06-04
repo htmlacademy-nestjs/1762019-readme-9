a2776fcc1b4121591f2e3d3cd36370c015da7f30
Alexander Lyzhin <al.lyzhin@gmail.com>
2025-06-05 02:01:26 +0300

    Добавит конфигурацию для работы с почтовым сервером Для подключения к почтовому серверу потребуются настройки. Их мы передаём через переменные окружения. По аналогии с другими сервисами подготовим конфигурацию.

--- project/libs/shared/helpers/src/index.ts
+++ project/libs/shared/helpers/src/index.ts
@@ -1,5 +1,4 @@
 export { fillDto } from './fill-dto';
-
 export { getMongoConnectionString } from './mongodb';
-
 export { getRabbitMQConnectionString, getRabbitMQOptions } from './rabbitmq';
+export { getMailerAsyncOptions } from './mail';
