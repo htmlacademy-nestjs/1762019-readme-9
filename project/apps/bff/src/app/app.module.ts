9ece4a4384fcd29499cf38be968e910911cc7fe5
Alexander Lyzhin <al.lyzhin@gmail.com>
2025-06-09 00:06:46 +0300

    Подключит HttpModule в app.module.ts. Подключим и сконфигурируем Axios. Для этого в `app.module.ts` подключим `HttpModule`. Укажем две настройки: `timeout` и `maxRedirects`.

--- project/apps/bff/src/app/app.module.ts
+++ project/apps/bff/src/app/app.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
+import { HttpModule } from '@nestjs/axios';
 
-import { BffConfigModule } from '@project/bff-config';
+import { bffConfig, BffConfigModule } from '@project/bff-config';
+import type { ConfigType } from '@nestjs/config';
 
 @Module({
-  imports: [BffConfigModule],
+  imports: [
+    BffConfigModule,
+    HttpModule.registerAsync({
+      useFactory: async (bffConfiguration: ConfigType<typeof bffConfig>) => {
+        return {
+          timeout: bffConfiguration.httpClientTimeout,
+          maxRedirects: bffConfiguration.httpClientMaxRedirects,
+        };
+      },
+      inject: [bffConfig.KEY],
+    }),
+  ],
   controllers: [],
   providers: [],
 })
